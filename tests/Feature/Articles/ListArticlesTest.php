659cfb07cf52d322a3d8afedcbf211cda7ce9e04
diega <diegochiola@gmail.com>
2024-03-20 15:53:29 +0100

    delete preexisting test and create first test by php artisan make test

---
+++ tests/Feature/Articles/ListArticlesTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListArticlesTest extends TestCase
+{
+    use RefreshDatabase;
+ /** @test **/
+    public function test_example(): void
+    {
+        //
+    }
+}
