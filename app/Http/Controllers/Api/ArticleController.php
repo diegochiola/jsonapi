a3eeba2e5d2316b9aa4db19e0db3e86d40f3f56a
diega <diegochiola@gmail.com>
2024-03-21 12:59:54 +0100

    corrections on name category

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     //
+    public function show(Article $article){
+        return $article;
+    }
 }
