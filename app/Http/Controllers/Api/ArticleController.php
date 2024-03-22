3f920af5a52509b6e99c5d93d97b94e68f6a0a95
diega <diegochiola@gmail.com>
2024-03-22 21:03:23 +0100

    finish files for this branch

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -14,8 +13,8 @@ class ArticleController extends Controller
     public function show(Article $article): ArticleResource{
         return ArticleResource::make($article);
     }
-    public function index(){
-        $articles = Article::all(); //queda pendiente paginarlos
+    public function index(): ArticleCollection{
+        //$articles = Article::all(); //queda pendiente paginarlos
         //como necesitamos una coleccion de articleResources
         return ArticleCollection::make(Article::all()); //cada uno de los articulos se envuela
 
