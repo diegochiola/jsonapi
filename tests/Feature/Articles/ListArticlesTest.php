d6a18f506c3bebedbe84b13dac6733ac108a38ab
diega <diegochiola@gmail.com>
2024-03-22 19:36:06 +0100

    create articles for test

--- tests/Feature/Articles/ListArticlesTest.php
+++ tests/Feature/Articles/ListArticlesTest.php
@@ -33,4 +33,52 @@ class ListArticlesTest extends TestCase
         ]);
 
     }
+
+public function can_fetch_all_articles(){
+    //primero crear articulos con factori
+    $articles = Article::factory()->count(3)->create();
+    $response = $this->getJson(route('api.v1.articles.index'));
+    $response->assertExactJson([
+        'data'=>[
+            [
+                'tupe'=> 'articles',
+                'id' => (string) $articles[0]->getRouteKey(),
+                'atributes' => [
+                    'title' => $articles[0]->title,
+                    'slug' => $articles[0]->slug,
+                    'content' => $articles[0]->content,
+                ],
+                'links' =>[
+                    'self' =>route('api.v1.articles.show', $articles[0])
+                ]
+            ],
+            [
+                'tupe'=> 'articles',
+                'id' => (string) $articles[1]->getRouteKey(),
+                'atributes' => [
+                    'title' => $articles[1]->title,
+                    'slug' => $articles[1]->slug,
+                    'content' => $articles[1]->content,
+                ],
+                'links' =>[
+                    'self' =>route('api.v1.articles.show', $articles[1])
+                ]
+            ],
+            [
+                'tupe'=> 'articles',
+                'id' => (string) $articles[2]->getRouteKey(),
+                'atributes' => [
+                    'title' => $articles[2]->title,
+                    'slug' => $articles[2]->slug,
+                    'content' => $articles[2]->content,
+                ],
+                'links' =>[
+                    'self' =>route('api.v1.articles.show', $articles[2])
+                ]
+            ]  
+        ]
+    ]);
+
+}
+
 }
